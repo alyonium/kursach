76d4190d8b3984954e81555a31f29b58ea70f43e
Alona <alyonium2.0@gmail.com>
2020-04-27 04:58:10 +0300

    it-works-better

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 
 #define N 6
 
-int mainLength = 740, mazeLength = 640, length = 64;
-int color = 0x0D1137, colorMaze = 0xE52165;
+int mainLength = 740, mazeLength = 640, length = 64, halfLength = 32;
+int colorBackground = 0x0D1137, colorField = 0xE52165;
 
 using namespace std;
 
@@ -33,26 +33,24 @@ public:
         this->bottom = bottom;
     }
 
-    void Wall (SDL_Surface *maze){
+    void Wall (SDL_Surface *field){
        if(this->top){
-            Draw_HLine(maze, x, y, x + length, color);
-            Draw_FillRect(maze, x - 1, y - 1, length + 2, 3, color);
+            Draw_FillRect(field, x - 1, y - 1, length + 2, 3, colorBackground);
         }
         if(this->bottom){
-            Draw_HLine(maze, x, y + length, x + length, color);
+            Draw_HLine(field, x, y + length, x + length, colorBackground);
         }
 
         if(this->left){
-            Draw_VLine(maze, x, y, y + length, color);
-            Draw_FillRect(maze, x - 1, y, 3, length + 2, color);
+            Draw_FillRect(field, x - 1, y, 3, length + 2, colorBackground);
         }
         if(this->right){
-            Draw_VLine(maze, x + length, y, y + length, color);
+            Draw_VLine(field, x + length, y, y + length, colorBackground);
         }
     }
 
-    void Final (SDL_Surface *maze) {
-        Draw_FillRect(maze, x + 2, y, length, length, 0x987439);
+    void Final (SDL_Surface *field){
+        Draw_FillRect(field, x + 2, y, length, length, 0x987439);
     }
 };
 
@@ -66,72 +64,68 @@ public:
         this->y = y;
     }
 
-    void Draw (SDL_Surface *maze, int a, int b){
-        Draw_FillCircle(maze, this->x, this->y, 20, colorMaze);
-        this->x = a;
-        this->y = b;
-        Draw_FillCircle(maze, this->x , this->y, 20, 0xffffff);
+    void Draw (SDL_Surface *field, int x, int y){
+        Draw_FillCircle(field, this->x, this->y, 20, colorField);
+        this->x = x;
+        this->y = y;
+        Draw_FillCircle(field, this->x , this->y, 20, 0xffffff);
     }
 };
 
-struct Cur { //индексы матрицы
+struct Current { //хранит индексы расположения в матрице текущей клетки
     int x;
     int y;
 };
 
-typedef struct Cur current;
+typedef struct Current position;
 
 
-int mazeDraw(SDL_Surface *screen, SDL_Surface *maze, SDL_Rect *frame_maze, SDL_Event event, Cell (*Maze)[10], current Current){
-    SDL_FillRect(maze, NULL, colorMaze);
-    for (int j = 0; j < 10; j++){
-        for (int i = 0; i < 10; i++){
-            Maze[i][j].Wall(maze);
+int Game(SDL_Surface *screen, SDL_Surface *field, SDL_Rect *frame_field, Cell (*Maze)[10]){
+    SDL_Event event;
+    SDL_FillRect(field, NULL, colorField);
+    position CurrentCell;
+    CurrentCell.x = 0;
+    CurrentCell.y = 0;
+    Maze[9][9].Final(field);
+    Character Mur(halfLength, halfLength);
+    Mur.Draw(field, halfLength, halfLength);
+        for (int j = 0; j < 10; j++){
+            for (int i = 0; i < 10; i++){
+                Maze[i][j].Wall(field);
+            }
         }
-    }
-    Current.x = 0;
-    Current.y = 0;
-
-    Maze[9][9].Final(maze);
-    Character Mur(32, 32);
-    Mur.Draw(maze, 32, 32);
-    SDL_Event new_event;
     while(SDL_WaitEvent(&event)){
                 if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_DOWN){
-                    if(Maze[Current.x][Current.y].bottom != 1){
-                        Mur.Draw(maze, Mur.x, Mur.y + 64);
-                        Current.y++;
+                    if(Maze[CurrentCell.x][CurrentCell.y].bottom != 1){
+                        Mur.Draw(field, Mur.x, Mur.y + length);
+                        CurrentCell.y++;
                     }
                 }
                 if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_UP){
-                    if(Maze[Current.x][Current.y].top != 1){
-                        Mur.Draw(maze, Mur.x, Mur.y - 64);
-                        Current.y--;
+                    if(Maze[CurrentCell.x][CurrentCell.y].top != 1){
+                        Mur.Draw(field, Mur.x, Mur.y - length);
+                        CurrentCell.y--;
                     }
                 }
                 if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RIGHT){
-                    if(Maze[Current.x][Current.y].right != 1){
-                        Mur.Draw(maze, Mur.x + 64, Mur.y);
-                        Current.x++;
+                    if(Maze[CurrentCell.x][CurrentCell.y].right != 1){
+                        Mur.Draw(field, Mur.x + length, Mur.y);
+                        CurrentCell.x++;
                     }
                 }
                 if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_LEFT){
-                    if(Maze[Current.x][Current.y].left != 1){
-                        Mur.Draw(maze, Mur.x - 64, Mur.y);
-                        Current.x--;
+                    if(Maze[CurrentCell.x][CurrentCell.y].left != 1){
+                        Mur.Draw(field, Mur.x - length, Mur.y);
+                        CurrentCell.x--;
                     }
                 }
-            SDL_BlitSurface(maze, NULL, screen, frame_maze);
-            SDL_Flip(screen);
-
-            if(SDL_PollEvent(&event)){
                 if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
-                     return 0;
+                    return 0;
+            SDL_BlitSurface(field, NULL, screen, frame_field);
+            SDL_Flip(screen);
             }
+}
 
-
-            }
-    }
 
 
 
@@ -140,9 +134,7 @@ int main(int argc, char** argv ){
     Cell Maze[10][10];
     SDL_Surface *screen, *maze;
     SDL_Rect frame_maze;
-    SDL_Event event;
     int exit;
-    current Current;
 //считываем данные из файла, в котором описывается расположение стен лабиринта
    ifstream in("walls.txt");
     if (in.is_open()) {
@@ -191,7 +183,7 @@ int main(int argc, char** argv ){
         in.close();
     }
     else {
-        cout << "ERROR";
+        cout << "ERROR OPENING FILE";
         return 1;
     }
 //работа с поверхностями
@@ -216,8 +208,8 @@ int main(int argc, char** argv ){
         return 4;
     }
 
-    SDL_FillRect(screen, NULL, color);
-    exit = mazeDraw(screen, maze, &frame_maze, event, Maze, Current);
+    SDL_FillRect(screen, NULL, colorBackground);
+    exit = Game(screen, maze, &frame_maze, Maze);
     if (exit == 0)
         SDL_Quit();
 
