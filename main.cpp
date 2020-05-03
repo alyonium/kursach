1c7c0a48443bd79c97e4153376ef8df3743bf8c7
Alona <alyonium2.0@gmail.com>
2020-05-04 00:00:15 +0300

    new-style

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 
 #define N 6
 
-int mainLength = 746, mazeLength = 643, length = 64, halfLength = 32; //прибавили к длинам основного окна и поля по 3, 6 пикселей для  симметрии
-int colorBackground = 0x039C7D, colorField = 0xB7D036, colorActive = 0xC91E2C, colorWall = 0xffffff; //фон, поле, персонаж + клетка выхода
+int mainLength = 746, mazeLength = 643, length = 64; //прибавили к длинам основного окна и поля по 3, 6 пикселей для  симметрии
+int colorField = 0xA0A0C0, colorActive = 0xE0A0C0, colorWall = 0x802080; //поле, клетка выхода, cтены
 static int counter = 0;
 
 using namespace std;
@@ -65,14 +65,14 @@ public:
 
     void Final (SDL_Surface *field){
         Draw_FillRect(field, x, y, length, length, colorActive); //картинка не во всю площадь клетки, поэтому прорисовываем тем же цветом заранее
-        SDL_Surface *goblet;
-        SDL_Rect frameGoblet;
-        frameGoblet.x = this->x + 6;
-        frameGoblet.y = this->y + 5;
-        frameGoblet.w = 56;
-        frameGoblet.h = 56;
-        goblet = SDL_LoadBMP("goblet (2).bmp");
-        SDL_BlitSurface(goblet, NULL, field, &frameGoblet);
+        SDL_Surface *scepter;
+        SDL_Rect frameScepter;
+        frameScepter.x = this->x + 6;
+        frameScepter.y = this->y + 5;
+        frameScepter.w = 56;
+        frameScepter.h = 56;
+        scepter = SDL_LoadBMP("scepter.bmp");
+        SDL_BlitSurface(scepter, NULL, field, &frameScepter);
     }
 
 
@@ -85,15 +85,15 @@ public:
         this->y = y;
     }
 
-    void Draw (SDL_Surface *field, SDL_Surface *harry, SDL_Rect *frameHarry, int x, int y){ //движение персонажа переименуй
-        SDL_FillRect (harry, NULL, colorField); //перекрасить по координатам рамки
-        SDL_BlitSurface(harry, NULL, field, frameHarry);
+    void Draw (SDL_Surface *field, SDL_Surface *sailor, SDL_Rect *frameSailor, int x, int y){ //движение персонажа переименуй
+        SDL_FillRect (sailor, NULL, colorField); //перекрасить по координатам рамки
+        SDL_BlitSurface(sailor, NULL, field, frameSailor);
         this->x = x;
         this->y = y;
-        frameHarry->x = x;
-        frameHarry->y = y;
-        harry = SDL_LoadBMP("harry.bmp");
-        SDL_BlitSurface(harry, NULL, field, frameHarry);
+        frameSailor->x = x;
+        frameSailor->y = y;
+        sailor = SDL_LoadBMP("sailor.bmp");
+        SDL_BlitSurface(sailor, NULL, field, frameSailor);
     }
 };
 
@@ -150,54 +150,53 @@ int readFile(Cell (&fieldMaze)[10][10]){  //считываем данные из файла, в котором
     return 0;
 }
 
-int messageWin(SDL_Surface *screen, SDL_Rect *frameField, SDL_Surface *field){
-    SDL_Surface *victory;
+int Win(SDL_Surface *screen, SDL_Rect *frameField, SDL_Surface *field){
+    SDL_Surface *rect;
     SDL_Surface *text = NULL;
-    SDL_Rect frameVictory;
+    SDL_Rect frameRect;
     SDL_Rect frameMessage;
     SDL_Event event;
     SDL_Color messageColor;
     TTF_Font *message = TTF_OpenFont("text.ttf", 30);
 
-    frameVictory.w = 320; frameVictory.h = 100;
-    frameVictory.x = 210; frameVictory.y = 320;
-    victory = SDL_CreateRGBSurface(SDL_HWSURFACE | SDL_DOUBLEBUF, frameVictory.w, frameVictory.h, 32, screen -> format -> Rmask, screen -> format -> Gmask, screen -> format -> Bmask, screen -> format -> Amask);
+    frameRect.w = 320; frameRect.h = 100;
+    frameRect.x = 210; frameRect.y = 320;
+    rect = SDL_CreateRGBSurface(SDL_HWSURFACE | SDL_DOUBLEBUF, frameRect.w, frameRect.h, 32, screen -> format -> Rmask, screen -> format -> Gmask, screen -> format -> Bmask, screen -> format -> Amask);
 
-    if (victory == NULL) { //поверхность прямоугольника для сообщения
+    if (rect == NULL) { //поверхность прямоугольника для сообщения
         cout << "SDL CREATE RGB SURFACE FAILED: %s\n" << SDL_GetError();
         atexit(SDL_Quit);
         return 4;
     }
 
-    SDL_FillRect(victory, NULL, colorActive);
-    SDL_BlitSurface(victory, NULL, screen, &frameVictory);
-    SDL_Flip(screen);
+    SDL_FillRect(rect, NULL, colorActive);
+    SDL_BlitSurface(rect, NULL, screen, &frameRect);
 
     if(message){
-        messageColor.r = 183;
-        messageColor.g = 208;
-        messageColor.b = 54;
+        messageColor.r = 128;
+        messageColor.g = 32;
+        messageColor.b = 128;
         frameMessage.x = 250;
         frameMessage.y = 352;
         text = TTF_RenderUTF8_Solid(message,"YOU ARE A WINNER", messageColor); //поверхность текста
         if(text){
-            Draw_Rect(victory, 0, 0, frameVictory.w, frameVictory.h, 0x000000); //черный контур вокруг сообщения
-            SDL_BlitSurface(victory, NULL, screen, &frameVictory);
+            Draw_Rect(rect, 0, 0, frameRect.w, frameRect.h, 0xA02080); //контур вокруг сообщения
+            SDL_BlitSurface(rect, NULL, screen, &frameRect);
             SDL_BlitSurface(text, NULL, screen, &frameMessage);
             SDL_Flip(screen);
       }
     }
+
     while(SDL_WaitEvent(&event)){
         if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)){
             TTF_CloseFont(message);
-
             return 0;
         }
     }
 }
 
-
 int Game(SDL_Surface *screen){
+
     Cell Maze[10][10];  //матрица расположения клеток
     if(readFile(Maze)){ //функция чтения файла
         cout << "ERROR OPENING FILE";
@@ -220,72 +219,71 @@ int Game(SDL_Surface *screen){
     Coordinates currentCell;
     Maze[9][9].Final(field);
 
-    Character Mur(3, 3);
+    Character Moon(3, 3);
 
-    SDL_Surface *harry;
-    SDL_Rect frameHarry;
-    harry = SDL_LoadBMP("harry.bmp");
-    frameHarry.x = 5;
-    frameHarry.y = 4;
-    frameHarry.w = 58;
-    frameHarry.h = 58;
-    SDL_BlitSurface(harry, NULL, field, &frameHarry);
+    SDL_Surface *sailor;
+    SDL_Rect frameSailor;
+    frameSailor.x = frameSailor.y = 3;
+    frameSailor.w = frameSailor.h = 58;
+    sailor = SDL_LoadBMP("sailor.bmp");
+    SDL_BlitSurface(sailor, NULL, field, &frameSailor);
 
-
-        for (int j = 0; j < 10; j++){
-            for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++)
+            for (int i = 0; i < 10; i++)
                 Maze[i][j].Wall(field);
-            }
-        }
     while(SDL_WaitEvent(&event)){
 
                 if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_LEFT){ //влево
                     if(Maze[currentCell.x][currentCell.y].left != 1){
-                        Mur.Draw(field,harry, &frameHarry, Mur.x - length, Mur.y);
+                        Moon.Draw(field, sailor, &frameSailor, Moon.x - length, Moon.y);
                         currentCell.x--;
                     }
                 } if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_UP){ //вверх
                     if(Maze[currentCell.x][currentCell.y].top != 1){
-                        Mur.Draw(field, harry, &frameHarry, Mur.x, Mur.y - length);
+                        Moon.Draw(field, sailor, &frameSailor, Moon.x, Moon.y - length);
                         currentCell.y--;
                     }
                 } if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RIGHT){ //вправо
                     if(Maze[currentCell.x][currentCell.y].right != 1){
-                        Mur.Draw(field, harry, &frameHarry, Mur.x + length, Mur.y);
+                        Moon.Draw(field, sailor, &frameSailor, Moon.x + length, Moon.y);
                         currentCell.x++;
                     }
                 } if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_DOWN){ //вниз
                     if(Maze[currentCell.x][currentCell.y].bottom != 1){
-                        Mur.Draw(field, harry, &frameHarry, Mur.x, Mur.y + length);
+                        Moon.Draw(field, sailor, &frameSailor, Moon.x, Moon.y + length);
                         currentCell.y++;
                     }
                 }
-                Draw_Rect(field, 0, 0, frameField.w, frameField.h, 0x000000); //черный контур
+
                 SDL_BlitSurface(field, NULL, screen, &frameField);
                 SDL_Flip(screen);
 
-                if(currentCell.x == 9 && currentCell.y == 9){ //это как-то работает но хуй пойми как. настоятельно рекомендую посмотреть еще раз, и обратить внимание на фрейм, потому что ширина и высота по умолчанию те же как у картинки (имеет ли смысл их прописывать тут)
-                        SDL_Surface *last = SDL_LoadBMP("last.bmp");
-                        SDL_Rect frameLast;
-                        frameLast.x = mazeLength - 64;
-                        frameLast.y = mazeLength - 67;
-                        frameLast.w = 61;
-                        frameLast.h = 64;
-                        SDL_BlitSurface(last, NULL, field, &frameLast);
-                        SDL_BlitSurface(field, NULL, screen, &frameField);
-                        SDL_Flip(screen);
-                    return messageWin(screen, &frameField, field); //вызываем поверхность с сообщением о победе
+                if(currentCell.x == 9 && currentCell.y == 9){
+                            SDL_Surface *win;
+                            SDL_Rect frameWin;
+                            frameWin.x = frameWin.y = 0;
+                            frameWin.w = frameWin.h = mainLength;
+                            win = SDL_CreateRGBSurface(SDL_HWSURFACE | SDL_DOUBLEBUF, frameWin.w, frameWin.h, 32, screen -> format -> Rmask, screen -> format -> Gmask, screen -> format -> Bmask, screen -> format -> Amask);
+
+                            if (win == NULL) {
+                                cout << "SDL CREATE RGB SURFACE FAILED: %s\n" << SDL_GetError();
+                                atexit(SDL_Quit);
+                                return 4;
+                            }
+
+                            win = SDL_LoadBMP("sailorwin.bmp");
+                            SDL_BlitSurface(win, NULL, screen, &frameWin);
+
+                            return Win(screen, &frameField, field); //вызываем поверхность с сообщением о победе
                 }
 
                 if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE){
-                 if(Menu(screen) != 3){
-                   return 0;
-                   }
+                    if(Menu(screen) != 3)
+                        return 0;
                 }
 
                 if (event.type == SDL_QUIT)
                     return 0;
-
             }
 }
 
@@ -293,31 +291,30 @@ int Menu(SDL_Surface *screen){
 
     TTF_Font *font = font = TTF_OpenFont("text.ttf", 30);
 
-    SDL_Surface *menu;
-    SDL_Rect frameMenu;
-    frameMenu.x = 187;
-    frameMenu.y = 53;
-    frameMenu.w = 372;
-    frameMenu.h = 643;
-
-    menu = SDL_CreateRGBSurface(SDL_HWSURFACE | SDL_DOUBLEBUF, frameMenu.w, frameMenu.h, 32, screen -> format -> Rmask, screen -> format -> Gmask, screen -> format -> Bmask, screen -> format -> Amask);
-
-
-    if (menu == NULL) { //поверхность прямоугольника для сообщения
-        cout << "SDL CREATE RGB SURFACE FAILED: %s\n" << SDL_GetError();
-        atexit(SDL_Quit);
-        return 4;
-    }
-    SDL_FillRect(menu, NULL, colorActive);
-    SDL_BlitSurface(menu, NULL, screen, &frameMenu);
+//    SDL_Surface *menu;
+//    SDL_Rect frameMenu;
+//    frameMenu.x = 187;
+//    frameMenu.y = 103;
+//    frameMenu.w = 372;
+//    frameMenu.h = 543;
+//
+//    menu = SDL_CreateRGBSurface(SDL_HWSURFACE | SDL_DOUBLEBUF, frameMenu.w, frameMenu.h, 32, screen -> format -> Rmask, screen -> format -> Gmask, screen -> format -> Bmask, screen -> format -> Amask);
+//    if (menu == NULL) { //поверхность прямоугольника для сообщения
+//        cout << "SDL CREATE RGB SURFACE FAILED: %s\n" << SDL_GetError();
+//        atexit(SDL_Quit);
+//        return 4;
+//    }
+//
+//    SDL_FillRect(menu, NULL, colorActive);
+//    SDL_BlitSurface(menu, NULL, screen, &frameMenu);
 
     int x, y;
     const int NUMMENU = 2;
     const char *items[NUMMENU] = {"PLAY", "EXIT"};
     SDL_Surface *choise[NUMMENU];
     bool select[NUMMENU] = {false, false};
-    SDL_Color color[2] = {{255, 255, 255}, //белый
-                          {0,   0,   255}}; //синий
+    SDL_Color color[2] = {{255, 255, 255},
+                          {64,   32,   128}};
 
     choise[0] = TTF_RenderText_Solid(font, items[0], color[0]);
     choise[1] = TTF_RenderText_Solid(font, items[1], color[0]);
@@ -401,9 +398,7 @@ int Menu(SDL_Surface *screen){
             SDL_BlitSurface(choise[i], NULL, screen, &pos[i]);
             SDL_Flip(screen);
         }
-
     }
-
 }
 
 
@@ -422,11 +417,20 @@ int main(int argc, char** argv ){
         return 3;
     }
 
-    SDL_Surface *surface = SDL_LoadBMP("fon.bmp");
-    SDL_Rect frameSurface;
-    frameSurface.x = frameSurface.y = 0;
+    SDL_Surface *background;
+    SDL_Rect frameBackground;
+    frameBackground.x = frameBackground.y = 0;
+    frameBackground.w = frameBackground.h = mainLength;
+    background = SDL_CreateRGBSurface(SDL_HWSURFACE | SDL_DOUBLEBUF, frameBackground.w, frameBackground.h, 32, screen -> format -> Rmask, screen -> format -> Gmask, screen -> format -> Bmask, screen -> format -> Amask);
 
-    SDL_BlitSurface(surface, NULL, screen, &frameSurface);
+    if (background == NULL) {
+        cout << "SDL CREATE RGB SURFACE FAILED: %s\n" << SDL_GetError();
+        atexit(SDL_Quit);
+        return 4;
+    }
+
+    background = SDL_LoadBMP("fon.bmp");
+    SDL_BlitSurface(background, NULL, screen, &frameBackground);
 
 
     if (SDLCALL TTF_Init()){
